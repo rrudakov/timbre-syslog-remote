994f378c03cffe2baca62b85f064a28ddf8eea46
Roman Rudakov <rrudakov@protonmail.com>
2022-05-30 21:49:36 +0200

    Update README

--- build.clj
+++ build.clj
@@ -1,12 +1,14 @@
 (ns build
   (:require
-   [clojure.tools.build.api :as b]))
+   [clojure.tools.build.api :as b]
+   [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'io.rrudakov/timbre-syslog-remote)
+(def lib 'net.clojars.rrudakov/timbre-syslog-remote)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def scm-url "git@github.com:rrudakov/timbre-syslog-remote.git")
 
 (defn clean [_]
   (println "Clean target dir")
@@ -18,7 +20,11 @@
                 :lib       lib
                 :version   version
                 :basis     basis
-                :src-dirs  ["src"]})
+                :src-dirs  ["src"]
+                :scm       {:tag                 "HEAD"
+                            :connection          (str "scm:git:" scm-url)
+                            :developerConnection (str "scm:git:" scm-url)
+                            :url                 scm-url}})
   (println "Copy sources and resources")
   (b/copy-dir {:src-dirs   ["src"]
                :target-dir class-dir})
@@ -29,5 +35,14 @@
 
 (defn release [_]
   (println "Release version" version)
-  (b/process {:command-args ["git" "tag" "--sign" version "-a" "-m" (str "Release " version)]})
+  (b/git-process {:git-args ["tag" "--sign" version "-a" "-m" (str "Release " version)]})
   (println "Success!"))
+
+(defn deploy [_]
+  (println "Deploy to Clojars")
+  (dd/deploy {:installer      :remote
+              :artifact       (b/resolve-path jar-file)
+              :pom-file       (b/pom-path {:lib       lib
+                                           :class-dir class-dir})
+              :sign-releases? true})
+  (println "Deployed successfully"))
